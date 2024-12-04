a14b14fab59aa9b1efbef74cafdd4405745db1d3
julianvansanten <julian@julianvansanten.nl>
2024-12-04 10:04:04 +0100

    Solve part 1 of day 4

--- src/Y2024/Day4/Day4.hs
+++ src/Y2024/Day4/Day4.hs
@@ -1,12 +1,42 @@
 module Y2024.Day4.Day4 (getDaySolutions) where
 
 
+import Data.List (transpose)
+import Data.Universe.Helpers (diagonals)
+
+
 getDaySolutions :: (String -> String, String -> String)
 getDaySolutions = (solve1, solve2)
 
 
 solve1 :: String -> String
-solve1 = error "First solution of day 4 not implemented yet!"
+solve1 = show . countTotal . lines
 
 solve2 :: String -> String
 solve2 = error "Second solution of day 4 not implemented yet!"
+
+
+-- | Count the total number of occurrances of XMAS in a matrix
+-- TODO: add diagonals
+countTotal :: [String] -> Int
+countTotal xs = horizontal xs + vertical xs + diagonal1 xs + diagonal2 xs
+    where
+        horizontal = sum . map countHorizontal
+        vertical = horizontal . transpose
+        diagonal1 ys = sum (map (countXmas . reverse) (diagonals ys)) + sum (map countXmas (diagonals ys))
+        diagonal2 = diagonal1 . transpose . map reverse
+
+
+-- | Count the number of occurrances of XMAS and SAMX in a String
+countHorizontal :: String -> Int
+countHorizontal xs = countXmas xs + countXmas (reverse xs)
+
+
+-- | Count the number of occurrances of XMAS in a String
+countXmas :: String -> Int
+countXmas [] = 0
+countXmas [_] = 0
+countXmas [_,_] = 0
+countXmas [_,_,_] = 0
+countXmas ('X':'M':'A':'S':xs) = 1 + countXmas xs
+countXmas (_:xs) = countXmas xs
---
+++ test/Y2024/Day4/Day4Spec.hs
@@ -0,0 +1,26 @@
+module Y2024.Day4.Day4Spec where
+
+
+import Y2024.Day4.Day4 (getDaySolutions)
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+
+spec :: Spec
+spec = do
+    test1
+    test2
+
+
+sample :: String
+sample = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX"
+
+
+test1 :: Spec
+test1 = describe "Y2024.Day4.Day4.solve1" $ do
+    it "Should solve the sample correctly" $ do
+        fst getDaySolutions sample `shouldBe` "18"
+
+
+test2 :: Spec
+test2 = describe "Y2024.Day4.Day4.solve2" $ do
+    it "is not implemented yet" $ False
