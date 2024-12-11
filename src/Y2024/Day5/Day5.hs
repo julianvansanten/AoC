37b31d5b52bc63c8998add5bcc13e533cbcdcd67
julianvansanten <julian@julianvansanten.nl>
2024-12-11 14:36:50 +0100

    Further attempt at day 5 part 2

--- src/Y2024/Day5/Day5.hs
+++ src/Y2024/Day5/Day5.hs
@@ -12,10 +12,10 @@ getDaySolutions = (solve1, solve2)
 
 
 solve1 :: String -> String
-solve1 = show . sum . map (\l -> l!!(length l `div` 2)) . filterConstraints . parsePrintQueue
+solve1 = show . sum . map (\l -> l!!(length l `div` 2)) . filterConstraints id . parsePrintQueue
 
 solve2 :: String -> String
-solve2 = error "Second solution of day 5 not implemented yet!"
+solve2 = show  . sum . map (\l -> l!!(length l `div` 2)) . getNumbers . fixPrintQueue . removePassing . parsePrintQueue
 
 
 parseAndShow :: String -> String
@@ -57,7 +57,12 @@ numbers = integer `sepBy` char ','
 
 -- | Data structure to store a print queue.
 data PrintQueue = PQ [(Int, Int)] [[Int]]
-    deriving Show
+    deriving (Eq, Show)
+
+
+-- | Get the list of lists with numbers in the PrintQueue.
+getNumbers :: PrintQueue -> [[Int]]
+getNumbers (PQ _ xss) = xss
 
 
 -- | Advent of Code show instance for PrintQueue.
@@ -66,8 +71,8 @@ instance AoCShow PrintQueue where
 
 
 -- | Apply a filter on the integer lists and return only those without broken constraints.
-filterConstraints :: PrintQueue -> [[Int]]
-filterConstraints (PQ cs xss) = filter (\xs -> getAll (mconcat (map (\c -> All (checkConstraint c xs)) cs))) xss
+filterConstraints :: (Bool -> Bool) -> PrintQueue -> [[Int]]
+filterConstraints f (PQ cs xss) = filter (\xs -> f (getAll (mconcat (map (\c -> All (checkConstraint c xs)) cs)))) xss
 
 
 -- | Given a constraint, check if a list of numbers is allowed.
@@ -76,3 +81,38 @@ checkConstraint _ [] = False
 checkConstraint _ [_] = True
 checkConstraint (f, s) (x:xs) | s == x = f `notElem` xs && checkConstraint (f, s) xs
     | otherwise = checkConstraint (f, s) xs
+
+
+-- | Remove all lists that do not break constraints.
+removePassing :: PrintQueue -> PrintQueue
+removePassing pq@(PQ cs _) = PQ cs (filter (/= []) (filterConstraints not pq))
+
+
+-- | Fix a single constraint on a list of numbers.
+fixConstraint :: (Int, Int) -> [Int] -> [Int]
+fixConstraint _ [] = []
+fixConstraint _ [x] = [x]
+fixConstraint (f, s) (x:xs) | s == x = f : x : filter (/= f) xs
+    | otherwise = x : fixConstraint (f, s) xs
+
+
+-- | For a list of constraints, filter all broken constraints for a given list.
+filterBrokenConstraints :: [Int] -> [(Int, Int)] -> [(Int, Int)]
+filterBrokenConstraints xs = filter (\c -> not (checkConstraint c xs))
+
+
+-- | Fix a PrintQueue once.
+fixOncePrintQueue :: PrintQueue -> PrintQueue
+fixOncePrintQueue (PQ cs xss) = PQ cs (map (\xs -> foldr fixConstraint xs (filterBrokenConstraints xs cs)) xss)
+
+
+-- | Check in an infinite list of `a`s for a fixed point.
+converge :: (a -> a -> Bool) -> [a] -> a
+converge p (x:ys@(y:_))
+    | p x y     = y
+    | otherwise = converge p ys
+converge _ _ = error "converge: finite list"
+
+-- | Fix a PrintQueue until it is fixed.
+fixPrintQueue :: PrintQueue -> PrintQueue
+fixPrintQueue = converge (==) . iterate fixOncePrintQueue
